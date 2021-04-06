2c424641a93b48cc8f91abce278afc9c18e95e84
CraftyGod <alegsex@gmail.com>
2021-04-07 01:59:09 +0300

    commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,8 +8,12 @@ class Task < ApplicationRecord
   # ]
 
   validates_presence_of :priority, numericality: { greater_than_or_equal_to: 0, less_than: 11, other_than: 0 }
-  validates_presence_of :due_date, :message => "Please select the due date"
+  validates_presence_of :due_date_cannot_be_in_the_past
 
+  def due_date_cannot_be_in_the_past
+    errors.add(:due_date, "can't be in the past") if
+      !due_date.blank? and due_date < Date.today
+  end
   def complete!
     if self.completed == false
       self.completed = true
