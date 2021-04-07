bebb42eb1d2a68501ac1339f14f8104954a72813
CraftyGod <alegsex@gmail.com>
2021-04-07 16:22:21 +0300

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_184318) do
+ActiveRecord::Schema.define(version: 2021_04_07_131637) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2021_04_03_184318) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstName"
+    t.string "lastName"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
