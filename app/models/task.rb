5fabf0f4004676f14748291211c910c8fbf65e8c
CraftyGod <alegsex@gmail.com>
2021-04-07 11:30:54 +0300

    commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,7 +13,7 @@ class Task < ApplicationRecord
   validates_presence_of :due_date_cannot_be_in_the_past
 
   def due_date_cannot_be_in_the_past
-    errors.add(:due_date, "can't be in the past") if
+    errors.add(:due_date, error: "can't be in the past") if
       !due_date.blank? and due_date < Date.today
   end
 
