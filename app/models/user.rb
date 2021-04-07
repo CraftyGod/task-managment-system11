e501b7e76949ab0604ddab4e921e30b3f6f407a7
CraftyGod <alegsex@gmail.com>
2021-04-07 16:57:48 +0300

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   self.primary_key = :id
 
-  validates_format_of :firstName, :with => /^[^0-9`!@#\$%\^&*+_=]+$/
-  validates_format_of :lastName, :with => /^[^0-9`!@#\$%\^&*+_=]+$/
   validates_presence_of :email, :password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
