b1407d2a3929eba053391015c281edf7a5844e15
CraftyGod <alegsex@gmail.com>
2021-04-08 09:18:04 +0300

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   self.primary_key = :id
 
-  validates_presence_of :email, :password
+  validates_presence_of :firstName, :lastName, :email, :password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
