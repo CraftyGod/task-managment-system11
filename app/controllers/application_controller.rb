592faf077c2debbc9714a66ed537c6d3dc9a21cc
CraftyGod <alegsex@gmail.com>
2021-04-12 01:27:55 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?; :require_login
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstName, :lastName, :email, :password, :password_confirmation, :current_password) }
   end
 
-  # before_action :require_login, :except => [:new, :create]
-  
+  before_action :authenticate_user!, :except => [:show, :index]
 
-  def require_login
-    unless user_signed_in?
-      redirect_to new_user_session_url
-    end
-  end
 end
