1556264b623f607b3da077bda07a8ef09191768c
CraftyGod <alegsex@gmail.com>
2021-04-12 16:54:53 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstName, :lastName, :email, :password, :password_confirmation, :current_password) }
   end
 
-  before_action :require_login, :except => [:new]
+  before_action :require_login, :except => [:new, :create]
 
   private
 
