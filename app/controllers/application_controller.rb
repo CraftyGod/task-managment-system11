e410b56b8a341ef57cdb43653d481666a352d912
CraftyGod <alegsex@gmail.com>
2021-04-12 19:25:53 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     unless user_signed_in?
-      redirect_to new_user_session_urlexcept
+      redirect_to new_user_session_url
     end
   end
 end
